7cbe912cb4a15535bbabaed376070adf158953b7
Karan Sharma <ks7585805@gmail.com>
2026-07-18 16:50:48 +0530

    Implement Number Guessing Game logic

--- Assignment1/NumberGuessingGame.java
+++ Assignment1/NumberGuessingGame.java
@@ -1,1 +1,27 @@
+import java.util.Scanner;
 
+public class NumberGuessingGame {
+    public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
+        int ans = 27;
+        boolean isGuessed = false;
+
+        for (int attempts = 1; attempts <= 5; attempts++) {
+            int guess = sn.nextInt();
+            if (guess < ans) {
+                System.out.println("Too Low");
+            } else if (guess > ans) {
+                System.out.println("Too High");
+            } else {
+                System.out.println("Congratulations! You guessed it.");
+                isGuessed = true;
+                break;
+            }
+        }
+
+        if (!isGuessed) {
+            System.out.println("Better Luck Next Time!");
+        }
+        sn.close();
+    }
+}
