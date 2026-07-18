1ab05c41a4ed9469c9322b8f7c8ea45e19634496
Karan Sharma <ks7585805@gmail.com>
2026-07-18 16:50:18 +0530

    Update PrimeNumbers.java

--- Assignment1/PrimeNumbers.java
+++ Assignment1/PrimeNumbers.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
 
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
+        int start = sn.nextInt();
+        int end = sn.nextInt();
+        for (int i = start; i <= end; i++) {
+            if (i <= 1) {
+                continue;
+            }
+            boolean isPrime = true;
+            for (int j = 2; j * j <= i; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.println(i);
+            }
+        }
+        sn.close();
+    }
+}
