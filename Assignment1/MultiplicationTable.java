38a976dbb543805f20fa95ccdf8afd147de9a974
Karan Sharma <ks7585805@gmail.com>
2026-07-18 16:49:49 +0530

    Add multiplication table generator in Java

--- Assignment1/MultiplicationTable.java
+++ Assignment1/MultiplicationTable.java
@@ -1,1 +1,15 @@
+import java.util.Scanner;
 
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
+        int n = sn.nextInt();
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Table of " + i);
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(i + " x " + j + " = " + (i * j));
+            }
+        }
+        sn.close();
+    }
+}
