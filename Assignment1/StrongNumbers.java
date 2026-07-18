71d7fecf29e19a024083303c9b8eec37f824571d
Karan Sharma <ks7585805@gmail.com>
2026-07-18 16:51:09 +0530

    Implement Strong Numbers detection in Java

--- Assignment1/StrongNumbers.java
+++ Assignment1/StrongNumbers.java
@@ -1,1 +1,27 @@
+import java.util.Scanner;
 
+public class StrongNumbers {
+    public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
+        int start = sn.nextInt();
+        int end = sn.nextInt();
+
+        for (int i = start; i <= end; i++) {
+            int temp = i;
+            int sum = 0;
+            while (temp > 0) {
+                int rem = temp % 10;
+                int fact = 1;
+                for (int j = 2; j <= rem; j++) {
+                    fact *= j;
+                }
+                sum += fact;
+                temp /= 10;
+            }
+            if (sum == i) {
+                System.out.println(i);
+            }
+        }
+        sn.close();
+    }
+}
