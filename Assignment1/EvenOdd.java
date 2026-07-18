a58dea78081559354f688db9fd9de0690b786f14
Karan Sharma <ks7585805@gmail.com>
2026-07-18 16:49:20 +0530

    Add EvenOdd class to check even and odd numbers

--- Assignment1/EvenOdd.java
+++ Assignment1/EvenOdd.java
@@ -1,1 +1,16 @@
+import java.util.Scanner;
 
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
+        int n = sn.nextInt();
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + " -> Even");
+            } else {
+                System.out.println(i + " -> Odd");
+            }
+        }
+        sn.close();
+    }
+}
